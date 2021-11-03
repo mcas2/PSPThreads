b1fed8e12fdc15dca12618ab380e654b19d90745
mcas2 <mcas2@tutanota.com>
2021-11-03 10:56:49 +0100

    Ejercicio 13 solucionado

--- src/EjerciciosUT2/Ejercicio13.java
+++ src/EjerciciosUT2/Ejercicio13.java
@@ -4,29 +4,29 @@ import java.io.*;
 
 public class Ejercicio13 implements Runnable {
     String notas;
+    Thread t;
 
     public Ejercicio13(String notas){
         this.notas = notas;
+        t = new Thread(this);
+        t.start();
     }
 
     @Override
     public void run() {
-        obtainGradesInString();
-        calculateMean();
+        System.out.println("Notas de " + obtainGradesInString()[0]);
+        System.out.println(calculateMean(obtainGradesInString()));
     }
 
     public String [] obtainGradesInString (){
         return notas.split(" ");
     }
 
-    public void readFile() {
-        String linea = "";
-        BufferedReader bf;
-
-        try {
-            bf = new BufferedReader(new FileReader("notas.txt"));
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
+    public double calculateMean (String[] notas){
+        Double media = 0.0;
+        for (int i = 1; i < notas.length; i++) {
+            media += Double.parseDouble(notas[i]);
         }
+        return media/notas.length;
     }
 }
--- src/EjerciciosUT2/Ejercicio13Main.java
+++ src/EjerciciosUT2/Ejercicio13Main.java
@@ -1,7 +1,23 @@
 package EjerciciosUT2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejercicio13Main {
     public static void main(String[] args) {
-        Ejercicio13 e13 = new Ejercicio13();
+        String linea = "";
+        BufferedReader bf;
+        try {
+            bf = new BufferedReader(new FileReader("notas.txt")); //Chuleta para el examen
+            while((linea = bf.readLine())!= null) {
+                Ejercicio13 e13 = new Ejercicio13(linea);
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
